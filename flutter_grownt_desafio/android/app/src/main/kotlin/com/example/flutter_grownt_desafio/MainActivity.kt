86412079b3afbe415df1778eaea306d652795800
Filipe Pires <filipepiresec@gmail.com>
2025-11-20 13:03:18 -0400

    first commit

---
+++ flutter_grownt_desafio/android/app/src/main/kotlin/com/example/flutter_grownt_desafio/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_grownt_desafio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_grownt_desafio/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_grownt_desafio/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
